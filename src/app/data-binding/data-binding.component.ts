7aa8629abe4a934d4c8e1ff068be056b246385c5
LSantosLS <lsantos.ls@outlook.com>
2023-03-13 20:49:23 -0300

    21. Property Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -11,6 +11,10 @@ export class DataBindingComponent {
   public idade: number = 26;
   public maisUm: number = 1;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbUx_Hkoe_NmAlbi_tkITeMxl9XJT8lEg3g&usqp=CAU";
+  public imgTitle: string = "Property Binding - Angular";
+
   constructor() {}
 
 }
