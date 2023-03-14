5cd6c351d596d02d2d7f1b56624d86815b37c3b7
LSantosLS <lsantos.ls@outlook.com>
2023-03-13 21:02:11 -0300

    22. Event Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -15,6 +15,17 @@ export class DataBindingComponent {
   public imgSrc: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBbUx_Hkoe_NmAlbi_tkITeMxl9XJT8lEg3g&usqp=CAU";
   public imgTitle: string = "Property Binding - Angular";
 
+  public position: {x: number, y:number} = {x: 0, y: 0};
+
   constructor() {}
+
+  public alterInfo(valor: MouseEvent) { // any
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent) { // any
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 
 }
