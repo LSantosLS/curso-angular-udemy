41afe2631fc3c8abb14b2a3027b5123a46125da6
LSantosLS <lsantos.ls@outlook.com>
2023-03-13 22:26:33 -0300

    26. *ngFor

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -10,6 +10,12 @@ export class DiretivasEstruturaisComponent implements OnInit {
   public condition: boolean = true;
   public conditionClick: boolean = true;
 
+  public list: Array<{nome: string, idade: number}> = [
+    {nome: "Luis Santos", idade: 26},
+    {nome: "Bruna Santos", idade: 25},
+    {nome: "Pedro Santos", idade: 23},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,5 +26,13 @@ export class DiretivasEstruturaisComponent implements OnInit {
 
   public onClick() {
     this.conditionClick = !this.conditionClick;
+  }
+
+  public onClickAddList() {
+    this.list.push({nome: "Daniel Ferreira", idade: 31});
+  }
+
+  public onClickEventList(event: number){
+    this.list.splice(event, 1);
   }
 }
