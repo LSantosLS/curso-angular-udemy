1998a20058fa3a474b8a7723a46da583dff56120
LSantosLS <lsantos.ls@outlook.com>
2023-03-14 20:42:47 -0300

    32. ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   template: `
     <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
-    <app-diretivas-atributos></app-diretivas-atributos>
+    
+    <app-diretivas-atributos>
+      <h1>Aulas de Diretivas e Atributos</h1>
+      <hr>
+    </app-diretivas-atributos>
+
+    <app-diretivas-atributos>
+      <h1>Teste</h1>
+      <h3>Este é um grande teste</h3>
+    </app-diretivas-atributos>
+
     <router-outlet></router-outlet>
   `,
 })
