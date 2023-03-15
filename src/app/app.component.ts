1511d97573ed78596bfd4857808dc87c889bd3fc
LSantosLS <lsantos.ls@outlook.com>
2023-03-14 21:46:31 -0300

    39. @Output() - Parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,11 @@ import { Component, OnInit } from '@angular/core';
 
     -->
 
-    <app-output></app-output>
+    <ng-template [ngIf]="getDados">
+      <h1> {{getDados.nome}} </h1>
+      <h2> {{getDados.idade}} </h2>
+    </ng-template>
+    <app-output (enviarDados)="setDados($event)"></app-output>
 
     <router-outlet></router-outlet>
   `,
@@ -34,6 +38,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   public addValue: number = 10;
+  public getDados: {nome: string, idade: number} | undefined;
 
   constructor() { }
 
@@ -41,6 +46,10 @@ export class AppComponent implements OnInit {
 
   public add() {
     this.addValue += 1;
+  }
+
+  public setDados(event: {nome: string, idade: number}) {
+    this.getDados = event;
   }
 
 }
